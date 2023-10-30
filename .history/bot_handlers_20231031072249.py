cebfc528239add05f8787b127c80a12cadf727d3
smartsoltech <a.choi@smartsoltech.kr>
2023-10-31 08:48:49 +0900

    megre

---
+++ .history/bot_handlers_20231031072249.py
@@ -0,0 +1,288 @@
+import telebot
+from telebot import types
+from db_operations import (
+    add_event,
+    add_reminder,
+    get_family_members,
+    add_reminder_to_db,
+    find_contact_by_name_or_phone,
+    get_reminders_by_family_member,
+    add_contact,
+    add_family_member,
+)
+from os import getenv
+from dotenv import load_dotenv
+from datetime import datetime
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+from db_operations import add_family_member as add_member_to_db
+from alembic import command
+from alembic.config import Config
+import os
+import subprocess
+import vobject
+from callback_handlers import (handle_event_description_input,
+                                handle_new_member_input)
+from logger import log_decorator
+# Import callback handlers
+from callback_handlers import handle_all_callbacks
+from main import user_sessions, print_user_sessions
+# user_sessions = {}  # словарь для хранения текущего состояния пользователя
+import json
+import random
+import string
+import qrcode
+
+def generate_unique_code():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+
+
+def prepare():
+    """
+    Prepare and load environment variables.
+    
+    Returns:
+        tuple: Telegram token and migration password
+    """
+    load_dotenv('.env')
+    token = getenv('TG_TOKEN')
+    MIGRATION_PASSWORD = getenv('MIGRATION_PASSWORD')
+    botname = getenv("BOT_NAME")
+    master_pwd = getenv("MASTER_PASSWORD")
+    return token, MIGRATION_PASSWORD, botname, master_pwd 
+
+token, MIGRATION_PASSWORD, bot_name, PASSWORD = prepare()
+bot = telebot.TeleBot(token)
+awaiting_password = set()  # Множество для отслеживания пользователей, ожидающих ввода пароля
+
+
+@log_decorator
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    handle_all_callbacks(bot, call)
+
+@log_decorator
+@bot.message_handler(commands=['migrate'])
+def migrate(message):
+
+    chat_id = message.chat.id
+    try:
+        password = message.text.split(' ')[1]
+    except IndexError:
+        bot.send_message(chat_id, "Please provide the migration password!")
+        return
+
+    if password == MIGRATION_PASSWORD:
+        try:
+            subprocess.check_call(['alembic', 'upgrade', 'head'])
+            bot.send_message(chat_id, "Migration successfully completed!")
+        except subprocess.CalledProcessError:
+            bot.send_message(chat_id, "Migration failed. Check server logs for details.")
+    else:
+        bot.send_message(chat_id, "Wrong migration password!")
+
+
+@log_decorator
+@bot.message_handler(commands=['addmember'])
+def add_member_link(message):
+    unique_code = generate_unique_code()
+
+    # Ссылка на вашего бота с параметром `start`
+    link = f"https://t.me/{bot_name}?start={unique_code}"
+    
+    # Сохранение уникального кода в user_sessions для последующей проверки
+    user_sessions[unique_code] = {
+        "action": "add_member",
+        "registration_open": True,
+        "first_name": "",
+        "last_name": "",
+        "username": "",
+        "chat_id": ""
+    }
+
+    # Генерация QR-кода
+    img = qrcode.make(link)
+    qrname=f"./tmp_files/{unique_code}.png"
+    img.save(qrname)
+    
+    with open(qrname, "rb") as file:
+        bot.send_photo(message.chat.id, file, caption=f"Use this QR code or [this link]({link}) to join the family.")
+
+    # Здесь можете отправить пользователю сообщение с инструкциями или запросить комментарий
+    bot.send_message(message.chat.id, f'Введите комментарий для этого пользователя!')
+
+ 
+    
+def save_family_member_name(message):
+    member_name = message.text
+    member_id = add_member_to_db(member_name)
+    bot.send_message(message.chat.id, f"Family member '{member_name}' added with ID {member_id}.")
+
+@bot.message_handler(commands=['start'])
+def start_handle(message):
+    chat_id = message.chat.id
+    user_data = message.from_user
+
+    # Проверка наличия кода в сообщении (пользователь перешел по инвайт-ссылке)
+    command, _, code = message.text.partition(' ')
+
+    if code and code in user_sessions and user_sessions[code]["registration_open"]:
+        # Извлечение данных пользователя
+        user_sessions[code]["first_name"] = user_data.first_name
+        user_sessions[code]["last_name"] = user_data.last_name or ""
+        user_sessions[code]["username"] = user_data.username
+        user_sessions[code]["chat_id"] = chat_id
+
+        # Закрыть регистрацию после успешного добавления
+        user_sessions[code]["registration_open"] = False
+
+        # Здесь можно добавить дополнительную логику или отправить сообщение пользователю о успешной регистрации
+        bot.send_message(chat_id, "You have been successfully registered!")
+        handle_new_member_input(bot, message)
+
+def text_handle(bot, message):
+  pass 
+
+    
+@log_decorator
+@bot.message_handler(commands=['getmember'])
+def get_family_member(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Please send the name of the family member you want to retrieve.")
+    bot.register_next_step_handler(message, send_family_member_info)
+    
+
+def send_family_member_info(message):
+    member_name = message.text
+    member = get_family_member_by_name(member_name)
+    if member:
+        bot.send_message(message.chat.id, f"Family member found: {member.name}")
+    else:
+        bot.send_message(message.chat.id, "Family member not found.")
+
+@bot.message_handler(commands=['addevent'])
+def add_new_event(message):
+    chat_id = message.chat.id
+    calendar, step = DetailedTelegramCalendar().build()
+    user_sessions[chat_id] = {"action": "add_event"}
+    bot.send_message(chat_id, f"Select {LSTEP[step]} for your event", reply_markup=calendar)
+
+@log_decorator
+@bot.message_handler(commands=['addreminder'])
+def add_new_reminder(message):
+    chat_id = message.chat.id
+    calendar, step = DetailedTelegramCalendar().build()
+    user_sessions[chat_id] = {"action": "add_reminder"}
+    bot.send_message(chat_id, f"Select {LSTEP[step]} for your reminder", reply_markup=calendar)
+
+@log_decorator
+@bot.message_handler(commands=['getreminders'])
+def show_reminders(message):
+    chat_id = message.chat.id
+    members = get_family_members()
+
+    if not members:
+        bot.send_message(chat_id, "You have no family members added!")
+        return
+
+    keyboard = types.InlineKeyboardMarkup()
+    for member in members:
+        callback_data = f"showreminders:{member.id}"
+        keyboard.add(types.InlineKeyboardButton(description=member.name, callback_data=callback_data))
+
+    bot.send_message(chat_id, "Select a family member to see their reminders:", reply_markup=keyboard)
+
+@log_decorator
+@bot.message_handler(content_types=['contact'])
+def handle_contact(message):
+    """Process received contact."""
+    chat_id = message.chat.id
+
+    # Extract contact details
+    full_name = f"{message.contact.first_name} {message.contact.last_name}" if message.contact.last_name else message.contact.first_name
+    telephone = message.contact.phone_number
+    names = full_name.split(' ', 1)
+    first_name = names[0]
+    last_name = names[1] if len(names) > 1 else ""
+
+    ask_to_save_contact(chat_id, first_name, last_name, telephone)
+    
+    
+def ask_to_save_contact(chat_id, first_name, last_name, telephone):
+    """Ask user whether to save parsed contact information."""
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    
+    data_to_send = json.dumps({
+        "action": "save_contact_yes",
+        "first_name": first_name,
+        "last_name": last_name,
+        "telephone": telephone
+    })
+    
+    yes_button = types.InlineKeyboardButton("Yes", callback_data=data_to_send)
+    no_button = types.InlineKeyboardButton("No", callback_data="save_contact_no")
+    keyboard.add(yes_button, no_button)
+    bot.send_message(chat_id, f"Do you want to save the contact?\nName: {first_name} {last_name}\nPhone: {telephone}", reply_markup=keyboard) 
+
+
+@log_decorator
+@bot.message_handler(commands=['full_reset'])
+def request_password(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Введите пароль для подтверждения действия:")
+    awaiting_password.add(chat_id)  # Добавляем пользователя в множество
+
+@bot.message_handler(func=lambda message: message.chat.id in awaiting_password)
+def check_password(message):
+    chat_id = message.chat.id
+    if message.text == PASSWORD:
+        # Если пароль верный, продолжаем с первоначальным действием
+        awaiting_password.remove(chat_id)  # Убираем пользователя из множества
+        full_reset(message)
+    else:
+        bot.send_message(chat_id, "Неверный пароль!")
+        awaiting_password.remove(chat_id)  # Убираем пользователя из множества
+
+def full_reset(message):
+    chat_id = message.chat.id
+    markup = types.InlineKeyboardMarkup()
+    yes_button = types.InlineKeyboardButton("Да, я уверен", callback_data="full_reset_confirm")
+    no_button = types.InlineKeyboardButton("Нет, отменить", callback_data="full_reset_cancel")
+    markup.add(yes_button, no_button)
+    bot.send_message(chat_id, "Вы уверены, что хотите полностью сбросить все таблицы? Это действие удалит все данные!", reply_markup=markup)
+
+## отлов остальных команд
+@log_decorator   
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    handle_all_callbacks(bot, call)
+    
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def handle_text(message):
+    chat_id = message.chat.id
+
+    if chat_id in user_sessions:
+        # Получаем текущее действие пользователя
+        action = user_sessions[chat_id].get("action")
+
+        if action == "add_event":
+            handle_event_description_input(bot, message)
+        elif action == "add_member":
+            handle_new_member_input(bot, message)
+        # добавьте другие проверки действий здесь по мере необходимости
+        else:
+            # Если неизвестное действие, обрабатываем как обычный текст
+            text_handle(bot, message)
+    else:
+        text_handle(bot, message)  # Обработка сообщений без конкретной сессии
+
+@bot.message_handler(func=lambda m: user_sessions.get(m.chat.id, {}).get("action") == "addmember")
+def save_new_member(message):
+    # Здесь вы можете получить user_code и другие необходимые данные из user_sessions или других источников
+    user_code = user_sessions.get(message.chat.id, {}).get("code")
+    first_name = message.text.split(' ', 1)[0]
+    last_name = message.text.split(' ', 1)[1] if ' ' in message.text else ""
+
+    # Здесь мы сохраняем нового члена семьи в базу данных
+    add_family_member(first_name, last_name, user_code, message.chat.id)
+    bot.send_message(message.chat.id, f"New member {first_name} {last_name} has been added.")
+    
---
+++ .history/callback_handlers_20231031084619.py
@@ -0,0 +1,241 @@
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+from telebot import types
+from db_operations import (get_family_members, 
+                           get_reminders_by_family_member, 
+                           add_contact, 
+                           full_reset, 
+                           add_event, 
+                           add_reminder,
+                           add_family_member)
+from db_operations import add_family_member, get_family_member_by_tid, get_family_members
+
+from logger import log_decorator
+from telebot.apihelper import ApiTelegramException
+import logging
+from main import user_sessions, initialize_user_session, print_user_sessions
+import json 
+
+logger = logging.getLogger(__name__)
+
+@log_decorator
+def handle_save_contact_query(bot, call, callback_data):
+    try:
+        first_name = callback_data["first_name"]
+        last_name = callback_data["last_name"]
+        telephone = callback_data["telephone"]
+        chat_id = call.message.chat.id
+        
+        add_contact(chat_id, telephone, first_name, last_name)
+        
+        bot.edit_message_text(
+            chat_id=chat_id, 
+            message_id=call.message.message_id, 
+            text=f"Контакт {first_name} {last_name} ({telephone}) был добавлен!"
+        )
+
+    except KeyError:
+        bot.answer_callback_query(call.id, "Ошибка при обработке данных контакта")
+
+        
+    bot.edit_message_text(chat_id=chat_id, message_id=call.message.message_id, text="Done!")
+    
+@log_decorator
+def handle_family_member_selection(bot, call):
+    member_id = call.data.split(":")[1]
+    chat_id = call.message.chat.id
+
+    if chat_id not in user_sessions:
+        bot.send_message(chat_id, "Произошла ошибка. Пожалуйста, попробуйте еще раз.")
+        return
+
+    user_sessions[chat_id]["family_member_id"] = member_id
+
+    bot.edit_message_text("Now, send the reminder description.",
+                          chat_id,
+                          call.message.message_id)
+
+
+@log_decorator
+def handle_calendar_callback(bot, call):
+    result, key, step = DetailedTelegramCalendar().process(call.data)
+    chat_id = call.message.chat.id
+    
+    if chat_id not in user_sessions or "action" not in user_sessions[chat_id]:
+        logger.error(f"No action found in user_sessions for chat_id: {chat_id}")
+        bot.send_message(chat_id, "Произошла ошибка. Пожалуйста, попробуйте еще раз.")
+        return
+    
+    if not result and key:
+        bot.edit_message_text(f"Select {LSTEP[step]}",
+                              chat_id,
+                              call.message.message_id,
+                              reply_markup=key)
+    elif result:
+        action = user_sessions[chat_id]["action"]
+        user_sessions[chat_id]["date"] = result
+
+        if action == "addevent":
+            bot.edit_message_text(f"You selected {result} for your event. Now send the event description.",
+                                  chat_id,
+                                  call.message.message_id)
+        elif action == "addreminder":
+            # After selecting a date, let the user choose a family member
+            members = get_family_members()
+            markup = types.InlineKeyboardMarkup()
+            for member in members:
+                markup.add(types.InlineKeyboardButton(member.name, callback_data=f"choose_member:{member.id}"))
+            bot.edit_message_text(f"You selected {result} for your reminder. Now choose a family member:",
+                                  chat_id,
+                                  call.message.message_id,
+                                  reply_markup=markup)
+
+@log_decorator
+def show_member_reminders(bot, call):
+    try:
+        member_id = call.data.split(":")[1]
+        reminders = get_reminders_by_family_member(member_id)
+        if reminders:
+            reminders_text = "\n".join([f"{reminder.id}. {reminder.text}" for reminder in reminders])
+            bot.edit_message_text(f"Reminders for member {member_id}:\n{reminders_text}", call.message.chat.id, call.message.message_id)
+        else:
+            bot.edit_message_text(f"No reminders found for member {member_id}.", call.message.chat.id, call.message.message_id)
+    except ApiTelegramException as e:
+        logger.error(f"Telegram API Error in show_member_reminders: {e}")
+    except Exception as e:
+        logger.error(f"Error in show_member_reminders: {e}")
+
+
+@log_decorator
+def handle_all_callbacks(bot, call):
+    try:
+        if call.data.startswith('showreminders:'):
+            show_member_reminders(bot, call)
+        
+        elif call.data in ["save_contact_yes", "save_contact_no"]:
+            handle_save_contact_query(bot, call)
+            if call.data == "save_contact_yes":
+                add_contact()
+        elif call.data == "full_reset_confirm":
+            full_reset()
+            
+            # bot.edit_message_text("Таблицы были успешно сброшены!", call.message.chat.id, call.message.message_id)
+        elif call.data == "full_reset_cancel":
+            bot.edit_message_text("Сброс отменен!", call.message.chat.id, call.message.message_id)
+
+        else:
+            logger.warning(f"Unknown callback data received: {call.data}")
+            bot.answer_callback_query(call.id, "Неизвестный запрос")
+    except ApiTelegramException as e:
+        logger.error(f"Telegram API Error while handling callback: {e}")
+    except Exception as e:
+        logger.error(f"Error handling callback: {e}")
+        
+    try:
+        callback_data = json.loads(call.data)
+        action = callback_data.get("action")
+        
+        if action == "save_contact_yes":
+            handle_save_contact_query(bot, call, callback_data)
+        # Добавьте другие условия обработки здесь по необходимости
+        else:
+            bot.answer_callback_query(call.id, "Неизвестный запрос")
+
+    except json.JSONDecodeError:
+        bot.answer_callback_query(call.id, "Некорректные callback данные")
+        
+    try:
+        if call.data.startswith('cbcal_'):
+            handle_calendar_callback(bot, call)
+
+        elif call.data.startswith('choose_member:'):
+            handle_family_member_selection(bot, call)
+    except Exception as e:
+        logger.error(f"Error handling callback: {e}")
+        
+        
+# def text_handle(bot, message):
+#     if message.text:
+#         bot.send_message(message.from_user.id, "Я не понимаю Вас!")
+        
+@log_decorator
+def handle_event_description_input(bot, message):
+    chat_id = message.chat.id
+    initialize_user_session(chat_id, "action")
+    print_user_sessions()
+    if chat_id not in user_sessions or "action" not in user_sessions[chat_id]:
+        logger.error(f"No action found in user_sessions for chat_id: {chat_id}")
+        bot.send_message(chat_id, "Произошла ошибка. Пожалуйста, попробуйте еще раз.")
+        return
+    
+    action = user_sessions[chat_id]["action"]
+    date = user_sessions[chat_id]["date"]
+    print_user_sessions()
+    if action == "addevent":
+        # Сохраняем событие в базу данных
+        add_event(message.text, date, chat_id)  # Предположим, что chat_id является ID члена семьи
+        bot.send_message(chat_id, "Ваше событие было добавлено!")
+
+    elif action == "addreminder":
+        # Сохраняем напоминание в базу данных
+        add_reminder(message.text, date, chat_id)  # Предположим, что chat_id является ID члена семьи
+        bot.send_message(chat_id, "Ваше напоминание было добавлено!")
+
+    # Удаляем информацию из user_sessions, чтобы предотвратить путаницу
+    del user_sessions[chat_id]
+    
+@log_decorator
+def handle_new_member_input(bot, message):
+    chat_id = message.chat.id
+    
+    print_user_sessions()
+    print(f'\n---------MESSAGE\n\n{message}\n\n')
+    # Создаем нового пользователя (или члена семьи) в базе данных
+    add_family_member(first_name=message.from_user.first_name, last_name=message.from_user.last_name, user_code=message.from_user.username, chat_id=chat_id, comment=message.text)
+    
+    bot.send_message(chat_id, "Новый пользователь был добавлен!")
+    
+
+def get_user_by_invite_id(user_code):
+    """
+    Извлекает информацию о пользователе по коду приглашения из базы данных.
+
+    Args:
+        user_code (str): Код приглашения.
+
+    Returns:
+        object: Объект FamilyMember с информацией о пользователе.
+    """
+    # Пользуемся уже определенной функцией
+    return get_family_member_by_tid(user_code)
+
+def user_exists_in_db(user_code, chat_id):
+    """
+    Проверяет, существует ли пользователь в базе данных по заданным user_code и chat_id.
+
+    Args:
+        user_code (str): Код приглашения пользователя.
+        chat_id (int): ID чата.
+
+    Returns:
+        bool: True, если пользователь найден, иначе False.
+    """
+    member = get_family_member_by_tid(user_code)
+    if member and member.chat_id == chat_id:
+        return True
+    return False
+
+def save_family_member(user_id, chat_id, first_name, last_name):
+    """
+    Сохраняет нового члена семьи в базу данных.
+
+    Args:
+        user_id (int): ID пользователя (может быть использован как код приглашения).
+        chat_id (int): ID чата.
+        first_name (str): Имя пользователя.
+        last_name (str): Фамилия пользователя.
+    """
+    # Проверяем, существует ли пользователь в базе данных
+    if not user_exists_in_db(user_id, chat_id):
+        add_family_member(first_name=first_name, last_name=last_name, user_code=user_id, chat_id=chat_id)
+    else:
+        print(f"User with user_code {user_id} and chat ID {chat_id} already exists.")
